c193ccd3155e6322c86cea05a6216bae273262c6
Alejandro <perezalejandro266@gmail.com>
2022-08-21 00:30:11 -0300

    Mejoras de movimiento, incorporacion de animaciones y mas

---
+++ Proyecto Final/Assets/Scripts/Player/Firebolt.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firebolt : MonoBehaviour
+{
+    [SerializeField] int Velocidad;
+
+    public int Damage;
+    [SerializeField]
+    [Range(1,5)]
+    float LiveTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DestroyBullet();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Movement();
+    }
+
+    void Movement()
+    {
+        transform.position = transform.position + Vector3.forward * Time.deltaTime * Velocidad;
+    }
+
+    void DestroyBullet()
+    {
+        Destroy(gameObject, LiveTime);
+    }
+}
--- Proyecto Final/Assets/Scripts/Player/PlayerMove.cs
+++ Proyecto Final/Assets/Scripts/Player/PlayerMove.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-
+    [SerializeField] Animator playerAnimator;
     [SerializeField] float Velocidad;
     private float cameraKey = 0f;
-
+    private Vector3 playerDirection;
+    
     [SerializeField] Transform Character;
     // Start is called before the first frame update
     void Start()
@@ -19,6 +20,7 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
         RotatePlayer();
+        AnimatePlayer();
         PlayerMovement();
     }
 
@@ -29,49 +31,49 @@ public class PlayerMove : MonoBehaviour
 
     void PlayerMovement()
     {
+        playerDirection = Vector3.zero;
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            Direction(Vector3.forward);
+        if (Input.GetKey(KeyCode.W)) playerDirection += Vector3.forward;
+       
+        if (Input.GetKey(KeyCode.S)) playerDirection += Vector3.back;
+       
+        if (Input.GetKey(KeyCode.A)) playerDirection += Vector3.left;
+       
+        if (Input.GetKey(KeyCode.D)) playerDirection += Vector3.right;
 
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            Direction(Vector3.back);
-
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            Direction(Vector3.left);
-
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            Direction(Vector3.right);
-
-        }
+        if (playerDirection != Vector3.zero) Direction(playerDirection);
 
     }
 
     void RotatePlayer()
     {
-        //cameraAxis += Input.GetAxis("Horizontal");
+        cameraKey += Input.GetAxis("Mouse X");
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        Quaternion newRotation = Quaternion.Euler(0, cameraKey, 0);
+        Character.rotation = Quaternion.Lerp(Character.rotation, newRotation, 3f * Time.deltaTime);
+
+    }
+
+    private bool IsAnimation(string animName)
+    {
+        return playerAnimator.GetCurrentAnimatorStateInfo(0).IsName(animName);
+    }
+
+    void AnimatePlayer()
+    {
+        bool forward = Input.GetKeyDown(KeyCode.W);
+        bool back = Input.GetKeyDown(KeyCode.S);
+        bool left = Input.GetKeyDown(KeyCode.A);
+        bool right = Input.GetKeyDown(KeyCode.D);
+       
+        if (forward) playerAnimator.SetTrigger("Run");
+        if (back) playerAnimator.SetTrigger("RunBack");
+        if (left) playerAnimator.SetTrigger("RunLeft");
+        if (right) playerAnimator.SetTrigger("RunRight");
+       
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
         {
-            cameraKey -= 1;
+            if (!IsAnimation("Idle")) playerAnimator.SetTrigger("Idle");
         }
-
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            cameraKey += 1;
-        }
-
-        //transform.rotation = Quaternion.Euler(0, cameraAxis, 0);
-
-        Character.rotation = Quaternion.Euler(0, cameraKey, 0);
     }
 }
